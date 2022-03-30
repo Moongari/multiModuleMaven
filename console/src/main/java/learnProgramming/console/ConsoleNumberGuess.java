3e2a336c5e47b6fe89a8657f7659f0d5521da836
Moustafa moungari <moungari.moustafa@gmail.com>
2022-03-30 16:56:41 +0200

    mise en place de logic pour jouer depuis la console

--- console/src/main/java/learnProgramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/learnProgramming/console/ConsoleNumberGuess.java
@@ -1,11 +1,15 @@
 package learnProgramming.console;
 
+import learnProgramming.Game;
+import learnProgramming.MessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class ConsoleNumberGuess {
@@ -13,9 +17,40 @@ public class ConsoleNumberGuess {
     //== logger fields ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
+    //== fields ==
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    //== events ==
     @EventListener(ContextRefreshedEvent.class)
     public void start(){
         log.info("start() --> container ready to use.");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if(game.isGameWon() || game.isGameLost()){
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("play again y/n ?");
+
+                String playAgainSgtring = scanner.nextLine().trim();
+                if(!playAgainSgtring.equalsIgnoreCase("y")){
+                    break;
+                }
+            }
+            game.reset();
+        }
 
     }
 
