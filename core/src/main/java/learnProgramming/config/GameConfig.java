3e3c605f18e100ee7365dbb7135bb9bcfd9a5811
Moustafa moungari <moungari.moustafa@gmail.com>
2022-04-04 14:30:03 +0200

    mise a place de fichier properties

--- core/src/main/java/learnProgramming/config/GameConfig.java
+++ core/src/main/java/learnProgramming/config/GameConfig.java
@@ -2,16 +2,26 @@ package learnProgramming.config;
 
 import learnProgramming.GuessCount;
 import learnProgramming.MaxNumber;
+import learnProgramming.MinNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
+@PropertySource("file:C:\\Users\\moungamo\\IdeaProjects\\multiModuleProjectMaven\\core\\src\\main\\resources\\application.properties")
 public class GameConfig {
 
     //== fields ==
-
-    private int maxNumber = 50;
-    private int guessCount = 8;
+    //=== define de default value : 20 ===
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+    //=== define de default value : 5 ===
+    @Value("${game.guessCount:5}")
+    private int guessCount;
+    @Value("${game.minNumber:5}")
+    private int minNumber;
 
 
 
@@ -27,5 +37,12 @@ public class GameConfig {
     public int guessCount(){
         return guessCount;
     }
+
+    @Bean
+    @MinNumber
+    public int minNumber(){
+        return minNumber;
+    }
+
 
 }
